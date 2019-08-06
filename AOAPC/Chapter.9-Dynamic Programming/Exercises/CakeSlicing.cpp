d80f3574f9ae82dc01bd7ba0f3063beb99a83596
Huasheng Hou <331292108@qq.com>
2019-08-06 08:31:38 +0800

    滑雪场的连续下降序列、切蛋糕

---
+++ AOAPC/Chapter.9-Dynamic Programming/Exercises/CakeSlicing.cpp
@@ -0,0 +1,35 @@
+//
+//  CakeSlicing.cpp
+//  AOAPC
+//
+//  Created by apple on 2019/8/5.
+//  Copyright © 2019 huasheng. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+static const int maxn = 20;
+
+class CakeSlicing {
+    int n, m, k, cherry[maxn*maxn];
+    
+public:
+    void solve() {
+        
+        while (cin >> n >> m >> k) {
+            
+            int c, r;
+            for (int i = 0; i < k; i ++) {
+                cin >> c >> r;
+            }
+        }
+    }
+    
+private:
+    int DP(int left, int right, int top, int bot) {
+        
+        return 0;
+    }
+};
--- AOAPC/Chapter.9-Dynamic Programming/Exercises/Snowboard.cpp
+++ AOAPC/Chapter.9-Dynamic Programming/Exercises/Snowboard.cpp
@@ -26,6 +26,7 @@ public:
         cin >> N;
         while (N) {
             N--;
+            memset(d, 0, sizeof(d));
             
             cin >> name >> R >> C;
             
@@ -38,9 +39,27 @@ public:
             int ans = 1;
             for (int i = 0; i < R; i++) {
                 for (int j = 0; j < C; j++) {
-                    cin >> m[i][j];
+                    ans = max(ans, DP(i, j));
                 }
             }
+            
+            cout << name << ": " << ans;
+            cout << "\n";
         }
+    }
+    
+private:
+    int DP(int r, int c) {
+        if (d[r][c] > 0) {
+            return d[r][c];
+        }
+        
+        int &ans = d[r][c]; ans = 1; int h = m[r][c];
+        if (r > 0 && m[r-1][c] < h) ans = max(ans, DP(r-1, c)+1);
+        if (r < R-1 && m[r+1][c] < h) ans = max(ans, DP(r+1, c)+1);
+        if (c > 0 && m[r][c-1] < h) ans = max(ans, DP(r, c-1)+1);
+        if (c < C-1 && m[r][c+1] < h) ans = max(ans, DP(r, c+1)+1);
+        
+        return ans;
     }
 };
